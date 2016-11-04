d5c5a361a356071163767e200790f7d451f3ed58
Dominic Tarr <dominic.tarr@gmail.com>
2016-11-04 15:24:16 +0000

    remove logging

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ var Obv = require('obv')
 var Drain = require('pull-stream/sinks/drain')
 
 module.exports = function (reduce) {
-  return function (log) {
+  return function (log, name) { //name is where this view is mounted
     var acc, since = Obv()
     since.set(-1)
     return {
       since: since,
       methods: {get: 'async'},
       get: function (path, cb) {
-        console.log("flumeview_reduce.get", path)
         cb(null, acc)
       },
       createSink: function (cb) {
         return Drain(function (data) {
-          console.log('flumeview_reduce', data)
           acc = reduce(acc, data.value, data.seq)
-          console.log('flumeview_reduce.acc', acc)
           since.set(data.seq)
         }, cb)
       },
@@ -26,5 +23,8 @@ module.exports = function (reduce) {
     }
   }
 }
+
+
+
 
 
