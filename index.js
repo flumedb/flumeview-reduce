fd0df2e29f3df64b39d59ab850909cb6725dcd09
Dominic Tarr <dominic.tarr@gmail.com>
2016-11-08 17:05:36 +0000

    flush any writes on close

--- index.js
+++ index.js
@@ -4,31 +4,38 @@ var AtomicFile = require('atomic-file')
 var path = require('path')
 var deepEqual = require('deep-equal')
 
+function isEmpty (o) {
+  for(var k in o) return false
+  return true
+}
+
 module.exports = function (reduce) {
   return function (log, name) { //name is where this view is mounted
     var acc, since = Obv()
-    var value = Obv(), _value
+    var value = Obv(), _value, int
 
     // if the log is persisted,
     // then also save the reduce state.
     // currently saving it every minute, if it's changed.
     // I don't think this is the best way
     // but it's the easiest...
-    if(log.dir) {
-      var state = AtomicFile(path.join(log.dir, name+'.json'))
+    if(log.filename) {
+      var dir = path.dirname(log.filename)
+      var state = AtomicFile(path.join(dir, name+'.json'))
       state.get(function (err, data) {
-        if(err) since.set(-1)
+        if(err || isEmpty(data)) since.set(-1)
         else {
           value.set(_value = data.value)
           since.set(data.seq)
         }
       })
       ;(function next() {
-        setTimeout(function () {
+        int = setTimeout(function () {
           if(since.value > 0 && !deepEqual(_value, value.value))
             state.set({seq: since.value, value: _value = value.value}, function () {})
           next()
-        }, 1000*60).unref()
+        }, 1000*60)
+        int.unref()
       })()
     }
     else
@@ -51,8 +58,18 @@ module.exports = function (reduce) {
         value.set(null); since.set(-1);
         if(state) state.set(null, cb)
         else cb()
+      },
+      close: function (cb) {
+        clearTimeout(int)
+        if(!since.value) return cb()
+        //force a write.
+        state.set({seq: since.value, value: _value = value.value}, cb)
       }
     }
   }
 }
+
+
+
+
 
