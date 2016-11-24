30bd752a42d522409779cc998ce0345ee6fc9856
Dominic Tarr <dominic.tarr@gmail.com>
2016-11-24 13:37:19 +0100

    use a different method to write fairly efficiently

--- index.js
+++ index.js
@@ -11,17 +11,38 @@ function isEmpty (o) {
 
 module.exports = function (reduce) {
   return function (log, name) { //name is where this view is mounted
-    var acc, since = Obv()
-    var value = Obv(), _value, int
+    var acc, since = Obv(), ts = 0
+    var value = Obv(), _value, writing = false, state, int
+
+    //if we are in sync, and have not written recently, then write the current state.
 
     // if the log is persisted,
     // then also save the reduce state.
-    // currently saving it every minute, if it's changed.
-    // I don't think this is the best way
-    // but it's the easiest...
+    // save whenever the view gets in sync with the log,
+    // as long as it hasn't beet updated in 1 minute.
+
+    function write () {
+      var _ts = Date.now()
+      if(state && since.value === log.since.value && _ts > ts + 60*1000 && !writing) {
+        clearTimeout(int)
+        int = setTimeout(function () {
+          ts = _ts; writing = true
+          state.set({seq: since.value, value: _value = value.value}, function () {
+            writing = false
+          })
+        }, 200)
+      }
+    }
+
+    //depending on the function, the reduction may not change on every update.
+    //but currently, we still need to rewrite the file to reflect that.
+    //(or accept that we'll have to reprocess some items)
+    //might be good to have a cheap way to update the seq. maybe put it in the filename,
+    //so filenames monotonically increase, instead of write to `name~` and then `mv name~ name`
+
     if(log.filename) {
       var dir = path.dirname(log.filename)
-      var state = AtomicFile(path.join(dir, name+'.json'))
+      state = AtomicFile(path.join(dir, name+'.json'))
       state.get(function (err, data) {
         if(err || isEmpty(data)) since.set(-1)
         else {
@@ -29,14 +50,6 @@ module.exports = function (reduce) {
           since.set(data.seq)
         }
       })
-      ;(function next() {
-        int = setTimeout(function () {
-          if(since.value > 0 && !deepEqual(_value, value.value))
-            state.set({seq: since.value, value: _value = value.value}, function () {})
-          next()
-        }, 1000*60)
-        int.unref()
-      })()
     }
     else
       since.set(-1)
@@ -46,12 +59,15 @@ module.exports = function (reduce) {
       value: value,
       methods: {get: 'async'},
       get: function (path, cb) {
+        if('function' === typeof path)
+          cb = path, path = null
         cb(null, value.value)
       },
       createSink: function (cb) {
         return Drain(function (data) {
           value.set(reduce(value.value, data.value, data.seq))
           since.set(data.seq)
+          write()
         }, cb)
       },
       destroy: function (cb) {
@@ -68,6 +84,19 @@ module.exports = function (reduce) {
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
