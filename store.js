1a1b012bcad7cdd2bc35c7c8b25b882c65fe7cf6
Dominic Tarr <dominic.tarr@gmail.com>
2017-05-21 19:51:18 +1200

    WIP

---
+++ store.js
@@ -0,0 +1,30 @@
+
+//var AtomicFile = require('atomic-file')
+
+//I was obviously thinking about browser stuff here
+//although this looks quite unfinished
+
+if(false && AtomicFile && !process.env.FV_REDUCE_LS) module.exports = AtomicFile
+else module.exports = function (key) {
+  return {
+    get: function (cb) {
+      setTimeout(function () {
+        cb(null, localStorage[key])
+      })
+    },
+    set: function (value, cb) {
+      setTimeout(function () {
+        try {
+          localStorage[key] = JSON.stringify(value)
+        } catch (err) {
+          return cb(err)
+        }
+        cb()
+      })
+    }
+  }
+}
+
+
+
+
