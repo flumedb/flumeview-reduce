b3b27a3db26e94ad8e7a0b6a999da5f3a125ba50
Dominic Tarr <dominic.tarr@gmail.com>
2017-08-04 22:40:12 +1200

    functional style

--- test/write.js
+++ test/write.js
@@ -59,3 +59,64 @@ tape('write before queue timeout', function (t) {
 })
 
 
+tape('write many times quickly', function (t) {
+
+  var s = Write.init()
+  t.equal(s.cleanTs, 0)
+  var s = Write.maybeWrite(s, 0, {}, 50)
+  var s = Write.maybeWrite(s, 1, {}, 100)
+  var s = Write.maybeWrite(s, 2, {}, 150)
+  var s = Write.maybeWrite(s, 3, {}, 200)
+
+  t.equal(s.dirty, true)
+  t.equal(s.dirtyTs, 200)
+  t.equal(s.since, 3)
+  t.equal(s.queue, true)
+  t.equal(s.write, false)
+
+  var s = Write.maybeWrite(s, 0, {}, 30e3+100)
+  //dirty time is updated, so next write time is pulled forward.
+  t.equal(s.dirty, true)
+  t.equal(s.dirtyTs, 30e3+100)
+  t.equal(s.since, 0)
+  t.equal(s.queue, true)
+  t.equal(s.write, false)
+
+  //timeout triggers early
+  s = Write.timeout(s, 30e3+201)
+  t.equal(s.write, false)
+  s = Write.timeout(s, 30e3+301)
+  t.equal(s.write, true)
+  t.end()
+})
+
+tape('write while writing', function (t) {
+  var s = Write.init()
+  t.equal(s.cleanTs, 0)
+  var s = Write.maybeWrite(s, 0, {}, 100)
+  t.equal(s.dirty, true)
+  t.equal(s.dirtyTs, 100)
+  t.equal(s.since, 0)
+  t.equal(s.queue, true)
+  t.equal(s.write, false)
+
+  //timeout triggers early
+  s = Write.timeout(s, 300)
+  t.equal(s.write, true)
+  t.equal(s.queue, false)
+  t.equal(s.dirty, false)
+  s = Write.maybeWrite(s, 0, {}, 400)
+  t.equal(s.write, true, 'writ?')
+  t.equal(s.queue, false, 'should not queue')
+  t.equal(s.dirty, true)
+  t.equal(s.dirtyTs, 400)
+
+  t.end()
+
+})
+
+
+
+
+
+
--- write.js
+++ write.js
@@ -9,24 +9,36 @@ function init () {
   }
 }
 
+// Test if now is a good time to write.
+// don't write if we are already writing
+// don't write if the view is not in sync with log
+// don't write if we already wrote in the last minute.
+// (note: this isn't stored, so doesn't effect the first write after process starts)
+
 function maybeWrite (state, since, value, ts) {
   state.dirty = true
   state.dirtyTs = ts
   state.queue = false
   state.since = since
   state.value = value
-
+  if(state.write)
+    
   if(state.ts + 60e3 > ts)
     return state
   state.queue = true
   return state
 }
 
+//don't actually start writing immediately.
+//incase writes are coming in fast...
+//this will delay until they stop for 200 ms
+
 function timeout (state, ts) {
   if(state.dirtyTs + 200 < ts && state.queue) {
     state.cleanTs = ts
     state.queue = false
     state.write = true
+    state.writing = true
     state.dirty = false
   }
   return state
@@ -35,6 +47,7 @@ function timeout (state, ts) {
 function written (state) {
   if(state.dirty)
     state.queue = true
+  state.writing = false
   return state
 }
 
@@ -48,68 +61,34 @@ function close (state, cb) {
 module.exports = function (state, version) {
   var int
     //write state
-//    var wState = {
-//      ts: 0, //last time written
-//      writing: false, //whether currently writing
-//      dirty: false,
-//      since: -1, //what sequence is persisted.
-//      value: null
-//    }
-//
     var wState = init()
 
-    // Test if now is a good time to write.
-    // don't write if we are already writing
-    // don't write if the view is not in sync with log
-    // don't write if we already wrote in the last minute.
-    // (note: this isn't stored, so doesn't effect the first write after process starts)
-    function _timeout () {
-      wState = timeout(wState, Date.now())
-      if(wState.write) {
-        state.set({seq: wState.since, version: version, value: value}, function () {
-          wState.writing = false
-          //if the state has changed while writing,
-          //consider another write.
-          if(wState.dirty) write()
-          else if(wState.cb) cb()
+    var effects = {
+      write: function _write () {
+        wState.write = false
+        state.set({seq: wState.since, version: version, value: wState.value}, function () {
+          cause({type: 'written', ts: Date.now()})
+        })
+      },
+      timer: function () {
+        clearTimeout(int)
+        int = setTimeout(function () {
+          cause({type: 'timeout', ts: Date.now() })
         })
       }
     }
 
+    Fun(
+
     function write (since, value) {
       if(!state) return //purely in memory.
       wState = maybeWrite(wState, since, value, Date.now())
-
-//      write.dirty = wState.dirty = true
-//      wState.since = since
-//      wState.value = value
-//      wState.dirtyTs = Date.now()
-//      var ts = Date.now()
-//      if(wState.writing) return
-//      if(wState.ts + 60e3 > ts) return
 
       if(wState.queue) {
         wState.queue = false
         clearTimeout(int)
         int = setTimeout(_timeout)
       }
-
-      //don't actually start writing immediately.
-      //incase writes are coming in fast...
-      //this will delay until they stop for 200 ms
-//      clearTimeout(int)
-//      int = setTimeout(function () {
-//        wState.clearTs = Date.now(); wState.writing = true
-//        wState.since = since
-//        write.dirty = wState.dirty = false
-//        state.set({seq: wState.since, version: version, value: value}, function () {
-//          wState.writing = false
-//          //if the state has changed while writing,
-//          //consider another write.
-//          if(wState.dirty) write()
-//          else if(wState.cb) cb()
-//        })
-//      }, 200)
     }
 
   write.close = function (cb) {
@@ -122,9 +101,33 @@ module.exports = function (state, version) {
   return write
 
 }
+
 module.exports.init = init
 module.exports.maybeWrite = maybeWrite
 module.exports.timeout = timeout
 module.exports.written = written
 module.exports.close = close
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
