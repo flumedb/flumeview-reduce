c48b14dbff6766e33499561d8aac8ada06098d7a
Dominic Tarr <dominic.tarr@gmail.com>
2017-08-08 20:29:36 +1200

    half finished

--- write.js
+++ write.js
@@ -1,3 +1,8 @@
+/*
+write after a leading edge but not a falling edge.
+
+*/
+
 function init () {
   return {
     cleanTs: 0,
@@ -15,119 +20,90 @@ function init () {
 // don't write if we already wrote in the last minute.
 // (note: this isn't stored, so doesn't effect the first write after process starts)
 
-function maybeWrite (state, since, value, ts) {
-  state.dirty = true
-  state.dirtyTs = ts
-  state.queue = false
-  state.since = since
-  state.value = value
-  if(state.write)
-    
-  if(state.ts + 60e3 > ts)
-    return state
-  state.queue = true
-  return state
-}
-
-//don't actually start writing immediately.
-//incase writes are coming in fast...
-//this will delay until they stop for 200 ms
-
-function timeout (state, ts) {
-  if(state.dirtyTs + 200 < ts && state.queue) {
-    state.cleanTs = ts
+var updaters = {
+  maybeWrite: function (state, update) {
+    state.dirty = true
+    state.dirtyTs = update.ts
     state.queue = false
-    state.write = true
-    state.writing = true
-    state.dirty = false
+    state.since = update.since
+    state.value = update.value
+    if(state.writing || state.ts + 60e3 > ts)
+      return {state: state}
+    return {state: state, effects: {type: 'queue'}}
+  },
+
+  //don't actually start writing immediately.
+  //incase writes are coming in fast...
+  //this will delay until they stop for 200 ms
+
+  timeout: function (state, ev) {
+    if(state.dirtyTs + 200 < ev.ts && state.queue) {
+      state.cleanTs = ev.ts
+      state.writing = true
+      state.dirty = false
+      return {state: state, effects: {type: 'write'}}
+    }
+    return state
+  },
+
+  written: function (state) {
+    state.writing = false
+    if(state.dirty)
+      return {state: state, effects: {type: 'queue'}}
+    return {state: state}
+  },
+
+  close: function (state) {
+    if(state.dirty && !state.writing)
+      return {state: state, effects: {type: 'write'}}
+    return {state: state}
   }
-  return state
 }
-
-function written (state) {
-  if(state.dirty)
-    state.queue = true
-  state.writing = false
-  return state
-}
-
-function close (state, cb) {
-  if(state.dirty)
-    state.write = true
-  return state
-}
-
 
 module.exports = function (state, version) {
   var int
     //write state
-    var wState = init()
+  var wState = init()
 
-    var effects = {
-      write: function _write () {
-        wState.write = false
-        state.set({seq: wState.since, version: version, value: wState.value}, function () {
-          cause({type: 'written', ts: Date.now()})
-        })
-      },
-      timer: function () {
-        clearTimeout(int)
-        int = setTimeout(function () {
-          cause({type: 'timeout', ts: Date.now() })
-        })
-      }
+  var effects = {
+    write: function _write (wState, _, cause) {
+      state.set({seq: wState.since, version: version, value: wState.value}, function () {
+        cause({type: 'written', ts: Date.now()})
+        if(cb && !wState.writing) cb()
+      })
+    },
+    queue: function (_, __, cause) {
+      clearTimeout(int)
+      int = setTimeout(function () {
+        cause({type: 'timeout', ts: Date.now() })
+      })
     }
+  }
 
-    Fun(
+  var cause = Fun(function update(state, event) {
+    return (updaters)[event.type](state, event)
+  }, function (state, effect, cause) {
+    effects[effect.type](state, effect, cause)
+  }, state)
 
-    function write (since, value) {
-      if(!state) return //purely in memory.
-      wState = maybeWrite(wState, since, value, Date.now())
-
-      if(wState.queue) {
-        wState.queue = false
-        clearTimeout(int)
-        int = setTimeout(_timeout)
-      }
-    }
+  function write (since, value) {
+    if(!state) return //purely in memory.
+    cause({
+      type: 'maybeWrite',
+      since: since,
+      value: value,
+      ts: Date.now()
+    })
+  }
 
   write.close = function (cb) {
-    if(!wState.dirty) return cb()
-    clearTimeout(int)
-    wState.cb = cb
-    write()
+    cause({type: 'close'})
+    if(!wState.writing) cb()
   }
 
   return write
 
 }
 
-module.exports.init = init
-module.exports.maybeWrite = maybeWrite
-module.exports.timeout = timeout
-module.exports.written = written
-module.exports.close = close
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports.updaters = updaters
 
