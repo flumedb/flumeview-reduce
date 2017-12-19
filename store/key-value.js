b3b5b163c9ffe1884018a607208f663a81eeff44
Michael LeGore <thelegore@gmail.com>
2017-12-19 18:10:13 -0500

    Switch to callbacks

--- store/key-value.js
+++ store/key-value.js
@@ -5,16 +5,10 @@ module.exports = function (KeyValueStore, version) {
     var keyValueStore = KeyValueStore(name, version)
     return {
       get: function (cb) {
-        keyValueStore.get(name).then(
-          val => cb(null, val),
-          err => cb(err)
-        )
+        keyValueStore.get(name, cb)
       },
       set: function (value, cb) {
-        keyValueStore.set(name, value).then(
-          val => cb(null, val),
-          err => cb(err)
-        )
+        keyValueStore.set(name, value, cb)
       }
     }
   }
