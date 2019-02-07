67b8fdc1ba53887c44cd07d0fff1e35532406e51
Dominic Tarr <dominic.tarr@gmail.com>
2019-02-08 00:28:53 +1300

    clear before running test

--- test/stream.js
+++ test/stream.js
@@ -3,7 +3,7 @@ var Reduce = require('../')
 var tape = require('tape')
 var Flume = require('flumedb')
 var pull = require('pull-stream')
-
+var rmrf = require('rimraf')
 
 var output = {}
 function log (name) {
@@ -20,18 +20,24 @@ module.exports = function (createFlume) {
       return createFlume()
       .use('view',
         Reduce("1", function (acc, item) {
+          console.log("REDUCE", acc, item)
           return (acc || 0) + 1
         }, null, null, 0)
       )
     }
     var db = create()
+    db.view.since.once(function () {
+      console.log("VIEW", db.since.value, db.view.since.value, db.view.value.value)
+    })
 
+    db.since.once(function () {
+      console.log("LOG", db.since.value, db.view.since.value, db.view.value.value)
+    })
     pull(
       db.view.stream({live: true}),
       pull.drain(log(1), function () {
       })
     )
-
     var i = 0
     var int = setInterval(function () {
       db.append({value: 10*(i++)}, function (err) {
@@ -63,7 +69,12 @@ module.exports = function (createFlume) {
 
 if(!module.parent)
   module.exports(function () {
+    rmrf.sync('/tmp/test-ssb-flumeview_offsetlog')
   //  return Flume(Log())
     return Flume(OffsetLog('/tmp/test-ssb-flumeview_offsetlog', {blockSize:1024, codec: require('flumecodec/json')}))
   })
+
+
+
+
 
