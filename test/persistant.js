053e8ae526bfe86e8c8616252764a4286e24161f
Dominic Tarr <dominic.tarr@gmail.com>
2019-03-25 22:25:35 +1300

    test persistance correctly

--- test/persistant.js
+++ test/persistant.js
@@ -1,10 +1,10 @@
 var Flume = require('flumedb')
 var FlumelogOffset = require('flumelog-offset')
 
-var file = '/tmp/test_flumeview-reduce/initial_'+Date.now()+'/log.offset'
+//var file = '/tmp/test_flumeview-reduce/initial_'+Date.now()+'/log.offset'
 
 require('./')(
-function create () {
+function create (file) {
   return Flume(FlumelogOffset(file, 1024, require('flumecodec/json')))
 })
 
