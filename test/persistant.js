5f2046c1c604853485adb3eeeb890faabdc90e12
Dominic Tarr <dominic.tarr@gmail.com>
2019-03-25 22:59:34 +1300

    fix test/persistent

--- test/persistant.js
+++ test/persistant.js
@@ -1,11 +1,12 @@
 var Flume = require('flumedb')
 var FlumelogOffset = require('flumelog-offset')
 
-//var file = '/tmp/test_flumeview-reduce/initial_'+Date.now()+'/log.offset'
+var file = '/tmp/test_flumeview-reduce/initial_'+Date.now()+'/log.offset'
 
 require('./')(
-function create (file) {
-  return Flume(FlumelogOffset(file, 1024, require('flumecodec/json')))
+function create () {
+  return Flume(FlumelogOffset(file, {block: 1024, codec: require('flumecodec/json')}))
 })
+
 
 
